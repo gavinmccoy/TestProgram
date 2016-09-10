782bfb8f7e34133e6b6f54f3f3a9c2eed6702817
gavinmccoy <gavin.mccoy2@mail.dcu.ie>
2016-09-10 18:57:35 +0100

    first commit

--- TestProgram/Module1.vb
+++ TestProgram/Module1.vb
@@ -2,6 +2,17 @@
 
     Sub Main()
 
-    End Sub
+        Console.WriteLine(Test.HowManyEs("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eanean sodales justo et Enim ornare, a congue lacus commodo."))
+        Console.WriteLine(Test.HowMany1s("17272838119191929838299111"))
+        Console.WriteLine(Test.Add(7, 5))
+        Console.ReadLine()
 
-End Module
+        'Outputs 
+        '12
+        '8
+        '12
+        'to the console.
+
+    End Sub ' Main
+
+End Module ' Module1
---
+++ TestProgram/Test.vb
@@ -0,0 +1,48 @@
+﻿Public Class Test
+
+    Public Shared Function HowManyEs(input As String) As Integer
+
+        ' Assumes letter e's mean any e regardless of case.
+
+        Dim count As Integer = 0
+
+        For Each item As Char In input.ToLower
+
+            If item = "e" Then
+
+                count += 1
+
+            End If
+
+        Next
+
+        Return count
+
+    End Function ' HowManyEs
+
+    Public Shared Function HowMany1s(input As String) As Integer
+
+        ' Put number in as string to prevent overflow.
+
+        Dim count As Integer = 0
+
+        For Each item As Char In input.ToLower
+
+            If item = "1" Then
+
+                count += 1
+
+            End If
+
+        Next
+
+        Return count
+
+    End Function ' HowMany1s
+
+    Public Shared Function Add(num1 As Integer, num2 As Integer) As Integer
+
+        Return num1 + num2
+
+    End Function ' Add
+End Class ' 
